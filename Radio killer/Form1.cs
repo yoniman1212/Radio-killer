179cbca43a6d8625145a99631722ca23c0628e3d
yoniman1212 <133513240+yoniman1212@users.noreply.github.com>
2023-07-19 08:42:19 -0700

    Add project files.

---
+++ Radio killer/Form1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Drawing;
+namespace Radio_killer
+{
+    public partial class Form1 : Form
+    {
+        public Color Color ;
+        public Form1()
+        {
+            InitializeComponent();
+    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
